0265fef23dbb64fed38ab2e848a9978a717c7fc6
mehdi-ra <mehdirahimi.dev@gmail.com>
2025-07-16 00:41:13 +0330

    tests: fixed problem with typescript ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,30 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/tests/**/*.test.ts'],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
-      tsconfig: 'tsconfig.test.json'
+      isolatedModules: true,
+      tsconfig: {
+        target: 'ES2020',
+        module: 'CommonJS',
+        lib: ['ES2020'],
+        outDir: 'dist-test',
+        strict: false,
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+        skipLibCheck: true,
+        forceConsistentCasingInFileNames: true,
+        resolveJsonModule: true,
+        moduleResolution: 'node',
+        types: ['jest', 'node'],
+        noEmit: true
+      }
     }],
+  },
+  moduleNameMapper: {
+    '^../src/(.*)$': '<rootDir>/src/$1'
   },
   collectCoverageFrom: [
     'src/**/*.ts',
@@ -20,5 +38,8 @@ module.exports = {
   ],
   clearMocks: true,
   resetMocks: true,
-  restoreMocks: true
+  restoreMocks: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  verbose: true
 };
