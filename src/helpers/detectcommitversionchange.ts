d5359333ff8e9f286892d41bca0258fa75fe8ee0
mehdi-ra <mehdirahimi.dev@gmail.com>
2025-07-16 02:29:38 +0330

    chore: test actions updated

--- src/helpers/detectcommitversionchange.ts
+++ src/helpers/detectcommitversionchange.ts
@@ -1,13 +1,15 @@
-import { isCandidateForMajorIncreaseRegEx, isCandidateForMinorIncreaseRegEx, isCandidateForPatchIncreaseRegEx } from '../const';
+import {
+  isCandidateForMajorIncreaseRegEx,
+  isCandidateForMinorIncreaseRegEx,
+  isCandidateForPatchIncreaseRegEx,
+} from '../const';
 import { TVersionChange } from '../interfaces/version';
 
 export function detectCommitVersionChange(
   commitMessage: string
 ): TVersionChange {
   const isCandidateForMajorIncrease =
-    isCandidateForMajorIncreaseRegEx.test(
-      commitMessage
-    );
+    isCandidateForMajorIncreaseRegEx.test(commitMessage);
 
   if (isCandidateForMajorIncrease) {
     return 'major';
